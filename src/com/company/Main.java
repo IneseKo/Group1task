5d68d380ac93ee47032ac55a64a98dc5ac42a820
bootcamp <www.dace.petersone@gmail.com>
2021-10-11 21:54:03 +0300

    Dace branch

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,10 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-
+        nameDace();
     }
-}
+    public static void nameDace (){
+            System.out.println("Hello Dace");
+        }
+    }
+
