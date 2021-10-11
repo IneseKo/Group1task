163e0a62edab2adc3856b4a2d5f074274e926c48
TatjanaAndreichuka <tatjana.andreichuka@gmail.com>
2021-10-11 21:54:35 +0300

    Crated Tatjana

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,10 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        tatjana();
+    }
 
+    public static void tatjana() {
+        System.out.println("Tatjana");
     }
 }
