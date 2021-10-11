e3dc567af2835eda353a3c7fbd90c0d869447447
IneseKo <in.kocina@gmail.com>
2021-10-11 23:48:50 +0300

    Added new Branch - Inese with a new method "nameInese"

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,11 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-
+        nameInese();
     }
+
+    public static void nameInese() {
+        System.out.println("Hello Inese!");
+    }
+
 }
